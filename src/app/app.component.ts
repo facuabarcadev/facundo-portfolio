79fe1b846e51aed20cff38ac29082bfa55a06315
facuabarcadev <facuabarca.dev@gmail.com>
2025-02-09 19:25:13 +0100

    feat(app): integrate UI components and apply background directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { GradientBackgroundDirective } from './directives/gradient-background.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
+  imports: [
+    HeaderComponent,
+    AboutComponent,
+    GradientBackgroundDirective,
+    ExperienceComponent,
+    ProjectsComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'facundo-portfolio';
