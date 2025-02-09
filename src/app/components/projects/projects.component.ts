92dd45f76c738790e9a55d76bfe137ccd6ec339c
facuabarcadev <facuabarca.dev@gmail.com>
2025-02-09 19:22:50 +0100

    feat(ui): add about, experience, footer, header, projects components

---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-projects',
+  templateUrl: 'projects.component.html',
+})
+export class ProjectsComponent {}
